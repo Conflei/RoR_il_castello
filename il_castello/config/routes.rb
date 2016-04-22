8f13751bef3eedb08b579fd292f707cbd24effde
unknown <VP50277@BPVMXL537202S.bpv.provincial.corp>
2016-04-22 18:49:53 -0430

    ejemploMiMetodo

--- il_castello/config/routes.rb
+++ il_castello/config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  get 'mimetodousuario/:usuario_id' => 'user#mimetodo', as: :mimetodo
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
