f52b029d3127f738223af3b16d36fc26bae0a3fe
Conflei <Felix1645018>
2016-04-24 03:23:06 -0430

    Sessions

--- il_castello/db/schema.rb
+++ il_castello/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419172013) do
+ActiveRecord::Schema.define(version: 20160424060747) do
 
   create_table "car_images", force: :cascade do |t|
     t.string   "id_Car"
@@ -47,6 +47,27 @@ ActiveRecord::Schema.define(version: 20160419172013) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "clients", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.boolean  "isAdmin"
+    t.string   "picture"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "clients", ["email"], name: "index_clients_on_email", unique: true
+  add_index "clients", ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+
   create_table "sections", force: :cascade do |t|
     t.string   "contentTitle"
     t.string   "contentDescription"
@@ -57,13 +78,24 @@ ActiveRecord::Schema.define(version: 20160419172013) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.string   "name"
     t.boolean  "isAdmin"
     t.string   "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
--- il_castello/test/controllers/user_test_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UserTestControllerTest < ActionController::TestCase
-  test "should get user_testing" do
-    get :user_testing
-    assert_response :success
-  end
-
-end
