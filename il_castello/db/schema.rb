dfb79b209ad706dcef9ab2bfd81bf6cf2bd195be
VCastle-01 <paramaplexd@gmail.com>
2016-04-27 00:46:05 -0430

    vista cms

--- il_castello/db/schema.rb
+++ il_castello/db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160426040751) do
 
-
   create_table "car_images", force: :cascade do |t|
     t.string   "id_Car"
     t.string   "picture"
@@ -23,17 +22,10 @@ ActiveRecord::Schema.define(version: 20160426040751) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "model"
+    t.string   "image_link"
     t.string   "description"
     t.boolean  "available"
     t.string   "id_User"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "catalagos", force: :cascade do |t|
-    t.string   "vehicle"
-    t.string   "description"
-    t.string   "photo"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
@@ -86,30 +78,30 @@ ActiveRecord::Schema.define(version: 20160426040751) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "name"
-    t.boolean  "isAdmin"
-    t.string   "picture"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
-
   create_table "templates", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.string   "urlImage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",     null: false
+    t.string   "encrypted_password",     default: "",     null: false
+    t.string   "name",                   default: "User", null: false
+    t.boolean  "isAdmin",                default: false,  null: false
+    t.string   "picture"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,      null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
